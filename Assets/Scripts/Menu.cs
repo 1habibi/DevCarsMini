256c5e890e469b979e20612f07f2d0f2d5202c15
sUnsetDHP <migunov.bbbb@gmail.com>
2022-05-09 17:44:22 +0300

    09.05.22 17.44

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,24 +7,42 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject storeMenu;
     [SerializeField] private GameObject levelsMenu;
+    [SerializeField] private GameObject earnCoins;
 
+    void Awake()
+    {
+        mainMenu.SetActive(true);
+        storeMenu.SetActive(false);
+        levelsMenu.SetActive(false);
+        earnCoins.SetActive(false);
+    }
     public void pushStore()
     {
         mainMenu.SetActive(false);
         storeMenu.SetActive(true);
         levelsMenu.SetActive(false);
+        earnCoins.SetActive(false);
     }
     public void pushBacktoMenu()
     {
+        mainMenu.SetActive(true);
         storeMenu.SetActive(false);
         levelsMenu.SetActive(false);
-        mainMenu.SetActive(true);
+        earnCoins.SetActive(false);
     }
     public void pushPlay()
     {
         mainMenu.SetActive(false);
         storeMenu.SetActive(false);
         levelsMenu.SetActive(true);
+        earnCoins.SetActive(false);
+    }
+    public void pushEarnCoins()
+    {
+        mainMenu.SetActive(false);
+        storeMenu.SetActive(false);
+        levelsMenu.SetActive(false);
+        earnCoins.SetActive(true);
     }
 
 }
